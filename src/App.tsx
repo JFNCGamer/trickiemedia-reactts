ede047f9821fe00078fdb9bb38c807a5e7753ef8
python4life32 <jfn.catarino@gmail.com>
2024-02-13 20:27:37 +0000

    h

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Video from "./Video";
 import About from "./About";
 import Nav from "./Nav";
 import Contacts from "./Contacts";
-import Services from "./Services";
 
 const App = () => {
   return (
