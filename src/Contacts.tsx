df8c508a5d4f4aecdaf57bcfee1cf075eec8ce9f
python4life32 <jfn.catarino@gmail.com>
2024-02-13 21:54:40 +0000

    CSS changes

--- src/Contacts.tsx
+++ src/Contacts.tsx
@@ -1,28 +1,34 @@
 const Contacts = () => {
   return (
-    <footer id="contactos" className=" m-auto text-center items-center">
-      <p className=" m-6 text-5xl font-poppins">Contactos</p>
+    <footer id="contactos" className=" m-auto ">
+      <p className=" m-6 text-5xl font-poppins text-center">Contactos</p>
 
       <div className="flex flex-col sm:flex-row justify-evenly mb-15 text-wrap">
         <div className="flex flex-col mb-2">
-          <a href="tel:965354682" className="text-xl mx-5">351+ 965354682</a>
-          <a className="text-xl mx-5 mt-4" href="mailto:geral.trickiemedia@gmail.com">geral.trickiemedia@gmail.com</a>
+          <div className="flex justify-center sm:justify-normal">
+            <h2 className="text-2xl font-poppins mr-2">Telefone:</h2>
+            <a href="tel:965354682" className="text-xl hover:text-slate-700 transition-colors">+351 965354682</a>
+          </div>
+          <div className="flex justify-center sm:justify-normal">
+            <h2 className="text-2xl font-poppins mr-2">Email:</h2>
+            <a href="mailto:geral.trickiemedia@gmail.com" className="text-xl hover:text-slate-700 transition-colors font-sans">geral.trickiemedia@gmail.com</a>
+          </div>
         </div>
         <div className="flex flex-row justify-center text-wrap">
           <a href="">
-          <button className="text-4xl m-5" title="Instagram" >
-            <i className="fa-brands fa-instagram"></i>
-          </button>
+            <button className="text-4xl m-5" title="Instagram">
+              <i className="fa-brands fa-instagram"></i>
+            </button>
           </a>
           <a href="">
-          <button className="text-4xl m-5" title="Linkedin">
-            <i className="fa-brands fa-linkedin"></i>
-          </button>
+            <button className="text-4xl m-5" title="Linkedin">
+              <i className="fa-brands fa-linkedin"></i>
+            </button>
           </a>
           <a href="">
-          <button className="text-4xl m-5" title="Vimeo">
-            <i className="fa-brands fa-vimeo"></i>
-          </button>
+            <button className="text-4xl m-5" title="Vimeo">
+              <i className="fa-brands fa-vimeo"></i>
+            </button>
           </a>
         </div>
       </div>
